5d872f8694e4f7f1f9ce54ce310821876be36194
Administrator <admin@example.com>
2020-03-05 13:28:43 +0800

    更新 seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 User.create(
     name: "root",
     email: "admin@seu.edu.cn",
@@ -8,15 +7,4 @@ User.create(
     password: "SEU-BME-CloudSelectPlatform",
     password_confirmation: "SEU-BME-CloudSelectPlatform",
     admin: true
-=======
-User.create(
-    name: "root",
-    email: "admin@seu.edu.cn",
-    num: "",
-    major: "",
-    department: "",
-    password: "SEU-BME-CloudSelectPlatform",
-    password_confirmation: "SEU-BME-CloudSelectPlatform",
-    admin: true
->>>>>>> c6e8b957a022596d021d9c2608d188695e478469
 )
